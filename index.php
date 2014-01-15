8436d3e464c2166dee33127602429d9773159912
Anas Ismail Khan <contact@anasismail.com>
2014-01-15 21:07:28 +0500

    Added a script or two attempting parse functions

--- index.php
+++ index.php
@@ -7,34 +7,3 @@
  */
 include('includes/page.php'); ?>
 
-<?php
-$url = 'http://username:password@hostname/path?arg=value#anchor';
-
-print_r(parse_url());
-
-echo parse_url($url, PHP_URL_PATH);
-?>
-----------------------------------------
-<?php
-function curPageURL() {
-    $pageURL = 'http';
-    if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
-    $pageURL .= "://";
-    if ($_SERVER["SERVER_PORT"] != "80") {
-        $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
-    } else {
-        $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-    }
-    return $pageURL;
-}
-?>
-<?php
-echo curPageURL();
-?>
-<?php
-function curPageName() {
-    return substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
-}
-
-echo "The current page name is ".curPageName();
-?>
