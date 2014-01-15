d2508511b4f5e3ad06133189940d592107c0d6c2
Anas Ismail Khan <contact@anasismail.com>
2014-01-15 22:10:48 +0500

    might have made a dent

---
+++ scripts/getPageContent.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anas
+ * Date: 1/15/14
+ * Time: 9:40 PM
+ */
+include("parse.php");
+function getPageContent($pagename){
+    $address = "../pages/".$pagename;
+    if(file_exists($address)){
+        return '<object id="board" type="text/html" data="'.$pagename.'.html"></object>';
+    }
+    else {
+        return "Error 404:<br>The Page: ".$pagename."was not found.";
+    }
+}
+echo getPageContent(getPageName());
+?>
