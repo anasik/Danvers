e8d1d9f0f1be5f1e39648d187d703491918fa318
Anas Ismail Khan <contact@anasismail.com>
2014-01-15 22:14:20 +0500

    did make the promised dent.. however still some debugging to do... wont get to the content file

--- scripts/getPageContent.php
+++ scripts/getPageContent.php
@@ -8,11 +8,12 @@
 include("parse.php");
 function getPageContent($pagename){
     $address = "../pages/".$pagename;
+    echo $address;
     if(file_exists($address)){
         return '<object id="board" type="text/html" data="'.$pagename.'.html"></object>';
     }
     else {
-        return "Error 404:<br>The Page: ".$pagename."was not found.";
+        return "Error 404:<br>The Page: ".$pagename.", was not found.<br>";
     }
 }
 echo getPageContent(getPageName());
