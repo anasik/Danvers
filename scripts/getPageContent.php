4c0d3c8cee8cf5852192c5a32ff8830006227988
Anas Ismail Khan <contact@anasismail.com>
2014-01-15 22:38:01 +0500

    debuggin.. no hope

--- scripts/getPageContent.php
+++ scripts/getPageContent.php
@@ -7,13 +7,13 @@
  */
 include("parse.php");
 function getPageContent($pagename){
-    $address = "../pages/{$pagename}.html";
-    echo $address;
+    $address = "../pages/{$pagename}";
     if(file_exists($address)){
         return '<object id="board" type="text/html" data="'.$pagename.'.html"></object>';
     }
     else {
-        return "Error 404:<br>The Page: {$pagename}, was not found.<br>";
+        return "Error 404:<br>The Page: ".$pagename}.", was not found.<br>";
     }
 }
+echo getPageContent(getPageName());
 ?>
