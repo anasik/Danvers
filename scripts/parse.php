eb5563d7ffdfeef8d7dae27608e366b5dddf3000
Anas Ismail Khan <contact@anasismail.com>
2014-05-24 22:59:04 +0500

    stupid move, not ready yet.. undone

--- scripts/parse.php
+++ scripts/parse.php
@@ -22,7 +22,7 @@ function getPageName() {
     return*/
     $url = GetPagesURL();
     $query = parse_url($url, PHP_URL_QUERY);
-    $page = substr($query, strpos($query,".com/") +4);
+    $page = substr($query, strpos($query,"page=") +5);
     return $page;
 
 }
