810fb5bf26bdc91b3530aef0cdc87165963327b6
Anas Ismail Khan <contact@anasismail.com>
2014-05-24 22:48:36 +0500

    Replaced index's code with page's

--- index.php
+++ index.php
@@ -5,6 +5,28 @@
  * Date: 1/14/14
  * Time: 9:29 PM
  */
-include('includes/page.php');
+//include('includes/page.php');
 ?>
+<?php include('../scripts/getPageContent.php');
+include('variables.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $SiteHeader;?></title>
+    <link rel="stylesheet" type="text/css" href=""/>
+</head>
+<body>
+<?php include('header.php');?>
+<?php include('nav.php');?>
+THE PAGE SHOULD FOLLOW
+<?php
+echo "<h3>".getPageName()."</h3><br>";
+//echo getPageContent(getPageName());
+echo "ASdsd"
+?>
+THE PAGE SHOULD END
+<?php include('sidebar.php');?>
+<?php include('footer.php');?>
+</body>
+</html>
 
