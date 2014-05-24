d3b0d8a02ef546ccb98a563bebf1ae96ab396ff2
Anas Ismail Khan <contact@anasismail.com>
2014-05-24 22:50:22 +0500

    Not gonnawork either way of course.. and i still prefer the previous

--- index.php
+++ index.php
@@ -7,26 +7,5 @@
  */
 //include('includes/page.php');
 ?>
-<?php include('../scripts/getPageContent.php');
-include('variables.php');?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title><?php echo $SiteHeader;?></title>
-    <link rel="stylesheet" type="text/css" href=""/>
-</head>
-<body>
-<?php include('header.php');?>
-<?php include('nav.php');?>
-THE PAGE SHOULD FOLLOW
-<?php
-echo "<h3>".getPageName()."</h3><br>";
-//echo getPageContent(getPageName());
-echo "ASdsd"
-?>
-THE PAGE SHOULD END
-<?php include('sidebar.php');?>
-<?php include('footer.php');?>
-</body>
-</html>
+
 
