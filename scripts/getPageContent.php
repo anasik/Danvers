8656c9619d3385bf30a9b240a0adfc8522d1dab4
Anas Ismail Khan <contact@anasismail.com>
2014-05-25 11:18:37 +0500

    manually reversed some changes*3

--- scripts/getPageContent.php
+++ scripts/getPageContent.php
@@ -9,7 +9,7 @@ include("parse.php");
 function getPageContent($pagename){
     $address = strtolower("pages/{$pagename}.html");
     if(file_exists($address)){
-        return '<div id="board">'.$address.'</div><br>';
+        return '<div id="board">'.file_get_contents($address).'</div><br>';
     }
         else {
         return "Error 404:<br>The Page: {$pagename}, was not found.<br>";
