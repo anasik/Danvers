0fcf65ed9c489ce87f2a66e291d4f4c5811b405c
Anas Ismail Khan <contact@anasismail.com>
2014-05-25 13:07:09 +0500

    added home url

--- includes/footer.php
+++ includes/footer.php
@@ -6,5 +6,7 @@
  * Time: 9:24 PM
  */ ?>
 <footer>
-    <span><p style="padding: 0 15px;" class="text-muted"><?php echo $footer;?></p></span>
+    <span><p style="telecommunication
+
+Engineering" class="text-muted"><?php echo $footer;?></p></span>
 </footer>
