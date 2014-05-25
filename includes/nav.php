9892e64b7a82aedab39dccaa564ae7bb9105f699
Anas Ismail Khan <contact@anasismail.com>
2014-05-25 09:02:30 +0500

    adding a nav

--- includes/nav.php
+++ includes/nav.php
@@ -4,4 +4,24 @@
  * User: anas
  * Date: 1/14/14
  * Time: 9:58 PM
- */ 
+ */?>
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">QuickSites</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href='<?php echo $address?>'>Home</a></li>
+            <li><a href='<?php echo $address?>'>About</a></li>
+            <li><a href='<?php echo $address?>'>Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
