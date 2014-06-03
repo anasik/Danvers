e9256ac0338c242fb7c93f2fc919bad2b2d4a168
Anas Ismail Khan <contact@anasismail.com>
2014-06-03 21:13:55 +0500

    eradicated all shit.. starting basic

--- scripts/theming.php
+++ scripts/theming.php
@@ -2,19 +2,19 @@
 $ThemeName = "default"; //would be the same as the name of the DIR with the themes files
 $ThemeDir = "themes/" . $ThemeName;
 $ThemeMarkup = file_get_contents($ThemeDir . "/markup.html");
+$AttrAddress = "../" . $ThemeDir . "/attributes.php";
+include $AttrAddress;
 
 function GetAttributes($tagName){
-    global $ThemeName, $ThemeDir;
-    include("../themes/default/attributes.php");
     global $ThemeAttrs;
-    if(array_key_exists($tagName, $ThemeAttrs)){
+    /*if(array_key_exists($tagName, $ThemeAttrs)){
         return $ThemeAttrs[$tagName];
     }
     else {
         return "nope";
     };
-};
+};*/
 echo $ThemeAttrs;
-echo GetAttributes("header");
+//echo GetAttributes("header");
 ?>
 
