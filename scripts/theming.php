d46f39a0e4607ca87495cf94e2e71f4c09ee4a79
Anas Ismail Khan <contact@anasismail.com>
2014-06-03 21:23:57 +0500

    stuck still

--- scripts/theming.php
+++ scripts/theming.php
@@ -2,19 +2,18 @@
 $ThemeName = "default"; //would be the same as the name of the DIR with the themes files
 $ThemeDir = "themes/" . $ThemeName;
 $ThemeMarkup = file_get_contents($ThemeDir . "/markup.html");
-$AttrAddress = "../" . $ThemeDir . "/attributes.php";
-include $AttrAddress;
-
+include("../". $ThemeDir . "/attributes.php");
+/*
 function GetAttributes($tagName){
-    global $ThemeAttrs;
-    /*if(array_key_exists($tagName, $ThemeAttrs)){
+    global $ThemeDir;
+    include($ThemeDir . "/attributes.php");
+    if(array_key_exists($tagName, $ThemeAttrs)){
         return $ThemeAttrs[$tagName];
     }
     else {
         return "nope";
-    };*/
-}
-echo $ThemeAttrs["header"];
-//echo GetAttributes("header");
+    }
+}*/
+echo $ThemeAttrs;
 ?>
 
