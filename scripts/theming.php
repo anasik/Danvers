8292cdc3f52e9c632ceaba0150abbe4872b2a268
Anas Ismail Khan <contact@anasismail.com>
2014-06-03 18:54:23 +0500

    added a func.. echoing

--- scripts/theming.php
+++ scripts/theming.php
@@ -3,4 +3,10 @@ $ThemeName = "default"; //would be the same as the name of the DIR with the them
 $ThemeDir = "themes/" . $ThemeName;
 $ThemeMarkup = file_get_contents($ThemeDir . "/markup.html");
 
+function GetAttributes($tagName){
+    global $ThemeDir;
+    include($ThemeDir. "/attributes.php");
+    return $ThemeAttrs[$tagName];
+}
+echo GetAttributes("header");
 ?>
