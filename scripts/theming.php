32353a27b08124270e6723dabceadf08aa43bf76
Anas Ismail Khan <contact@anasismail.com>
2014-06-03 20:11:06 +0500

    made changes to the func and shit. hope it works now

--- scripts/theming.php
+++ scripts/theming.php
@@ -2,11 +2,17 @@
 $ThemeName = "default"; //would be the same as the name of the DIR with the themes files
 $ThemeDir = "themes/" . $ThemeName;
 $ThemeMarkup = file_get_contents($ThemeDir . "/markup.html");
+include($ThemeDir . "/attributes.php");
 
 function GetAttributes($tagName){
-    global $ThemeDir;
-    include($ThemeDir. "/attributes.php");
-    return $ThemeAttrs[$tagName];
+    global $ThemeAttrs;
+    if(array_key_exists($tagName, $ThemeAttrs)){
+        return $ThemeAttrs[$tagName];
+    }
+    else {
+        return "nope";
+    }
 }
 echo GetAttributes("header");
 ?>
+
