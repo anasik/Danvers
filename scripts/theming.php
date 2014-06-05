1bce023cc41f426813720c75885f7cdd1846608a
Anas Ismail Khan <contact@anasismail.com>
2014-06-05 09:50:00 +0500

    damn the attempts.. removed the include in function, globalled both DIR and ATTRS

--- scripts/theming.php
+++ scripts/theming.php
@@ -5,8 +5,7 @@ $ThemeMarkup = file_get_contents($ThemeDir . "/markup.html");
 include("../". $ThemeDir . "/attributes.php");
 
 function GetAttributes($tagName){
-    global $ThemeDir//, $ThemeAttrs;
-    include("../". $ThemeDir . "/attributes.php");
+    global $ThemeDir, $ThemeAttrs;
     if(array_key_exists($tagName, $ThemeAttrs)){
         return $ThemeAttrs[$tagName];
     }
