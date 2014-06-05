aca580c589a8faa4db205fd8af109279fc479d31
Anas Ismail Khan <contact@anasismail.com>
2014-06-05 10:03:22 +0500

    applied the default theme again.. + added func to header tag.. hope it works now...*2 didnt work as expected.. perhaps the address.. lemme alter and try

--- scripts/theming.php
+++ scripts/theming.php
@@ -2,7 +2,8 @@
 $ThemeName = "default"; //would be the same as the name of the DIR with the themes files
 $ThemeDir = "themes/" . $ThemeName;
 $ThemeMarkup = file_get_contents($ThemeDir . "/markup.html");
-include("../". $ThemeDir . "/attributes.php");
+//include("../". $ThemeDir . "/attributes.php");
+include($ThemeDir . "/attributes.php");
 
 function GetAttributes($tagName){
     global $ThemeAttrs;
