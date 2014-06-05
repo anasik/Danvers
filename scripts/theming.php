0489b821e58bf4b73046152308842814badfb781
Anas Ismail Khan <contact@anasismail.com>
2014-06-05 09:43:37 +0500

    some success.. now it does reach the file.. however, teh function still has to be tested... hoping it wont take much time

--- scripts/theming.php
+++ scripts/theming.php
@@ -3,9 +3,9 @@ $ThemeName = "default"; //would be the same as the name of the DIR with the them
 $ThemeDir = "themes/" . $ThemeName;
 $ThemeMarkup = file_get_contents($ThemeDir . "/markup.html");
 include("../". $ThemeDir . "/attributes.php");
-/*
+
 function GetAttributes($tagName){
-    global $ThemeDir;
+    global $ThemeDir, $ThemeAttrs;
     include($ThemeDir . "/attributes.php");
     if(array_key_exists($tagName, $ThemeAttrs)){
         return $ThemeAttrs[$tagName];
@@ -13,8 +13,9 @@ function GetAttributes($tagName){
     else {
         return "nope";
     }
-}*/
-echo "../". $ThemeDir . "/attributes.php";
-echo $ThemeAttrs["header"];
+}
+//echo "../". $ThemeDir . "/attributes.php";
+//echo $ThemeAttrs["header"];
+echo GetAttributes("header");
 ?>
 
