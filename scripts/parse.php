60c0377646696d9a11baa40f863b5517bf6598a6
Haider Ali Khichi <khichihaider@gmail.com>
2016-02-28 13:48:52 +0500

    Fixed an error - Added a semicolon
    I like this boilerplate idea. Thought I'd contribute.

--- scripts/parse.php
+++ scripts/parse.php
@@ -2,7 +2,7 @@
 function GetPagesURL() {
     $pageURL = 'http';
   //  if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
-	if(!empty($_SERVER["HTTPS"])) {$pageURL .= "s"}
+	if(!empty($_SERVER["HTTPS"])) {$pageURL .= "s";}
     $pageURL .= "://";
     if ($_SERVER["SERVER_PORT"] != "80") {
         $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
