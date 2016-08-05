e9976b82de41d7ff0bd3d3da7673397b6640d26d
Anas Ismail Khan <contact@anasismail.com>
2016-08-05 16:32:23 +0500

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -2,6 +2,7 @@
 ?><nav class="navbar navbar-inverse">
           <ul class="nav navbar-nav">
             <li><a href="<?php echo $homeURL;?>">Home</a></li>
+            <li><a href="?page=blog">Blog</a></li>
             <li><a href="?page=about">About</a></li>
             <li><a href="?page=contact">Contact</a></li>
           </ul>
